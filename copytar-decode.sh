b84a0b82cd1cfc2dc93fcd406a1b72fef5fc5445
Laurent Ghigonis <laurent@gouloum.fr>
2015-02-19 20:01:59 +0100

    copytar-decode: more verbose

--- copytar-decode.sh
+++ copytar-decode.sh
@@ -9,8 +9,9 @@ function copytar_decode() {
 
     CTFILE=/tmp/copytar.encode
     sed "s,\x1B\[[0-9;]*[a-zA-Z],,g" $* | egrep "^COPYTAR" | tr -d '\r' | while read header content; do
+        echo "COPYTAR archive found !"
         echo $content | base64 -d > $CTFILE
-        tar -xf $CTFILE
+        tar -xvf $CTFILE
     done
     rm  $CTFILE
 
